5eebd9dd41f890fe1a2f95386bde9ebd2c981de1
liadkeller <liadkeller@gmail.com>
2019-10-31 19:56:35 +0200

    Preserve caller activity in the back stack

--- app/src/main/java/com/liadk/android/pushit/ResetPasswordFragment.java
+++ app/src/main/java/com/liadk/android/pushit/ResetPasswordFragment.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.NavUtils;
 import android.util.Log;
 import android.util.Patterns;
 import android.view.LayoutInflater;
@@ -86,7 +85,7 @@ public class ResetPasswordFragment extends Fragment {
     private void onResetSuccess() {
         Log.d(TAG, "resetPassword:success");
         Toast.makeText(getActivity(), R.string.password_reset, Toast.LENGTH_LONG).show();
-        NavUtils.navigateUpFromSameTask(getActivity());
+        getActivity().finish();
     }
 
     private void onResetFailed(Exception exception, String email) {
